2f8322da07e75d7eb4c3e5ebf70512b46be12d6e
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-10-01 12:24:10 +0200

    fixing checks

--- R/check_params.R
+++ R/check_params.R
@@ -2,11 +2,14 @@
 #'
 #' This functions checks if the parameters of a given function
 #' belong to the pre-defined parameter list.
+#' @param fun_name \code{character} string; the name of the function.
+#' @param parameters \code{character} vector; acceptable parameter names.
+#' @export
 #' @examples
 #' f1 <- function(x = 1)
 #'   x
 #'
-#' check_args("f1", "x")
+#' check_params("f1", "x")
 
 check_params <- function(fun_name, parameters) {
   res <- all(setdiff(names(as.list(args(fun_name))), "") %in% parameters)
